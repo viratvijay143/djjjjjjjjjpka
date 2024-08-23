cefbd4a9b9919f3ebcb69b184674357b5634e3bd
SAHU <161798305+viratvijay143@users.noreply.github.com>
2024-08-23 07:01:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ API_ID = API_ID = 20407127
 
 API_HASH = os.environ.get("API_HASH", "877e7db614293fb2d7c5e4caa79896f7")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7399984545:AAGTmSshdHpCpQEiHR3U4RiprUyuY5PdKf0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7417717621:AAFibMWs3uejBzUPJrljDww0zPq7NyQQo1M")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 5128979564))
+OWNER = int(os.environ.get("OWNER", 5498931783))
 
-LOG = -1002047255106
+LOG = -1002116155974
 
 # UPDATE_GRP = , # bot sat group
 
